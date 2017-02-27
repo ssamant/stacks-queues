c6a3923b4a63913360555ffb7d7a7bb01dce7536
sai samant <saisamant@gmail.com>
2017-02-26 20:02:12 -0800

    code done and passes specs

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,29 @@
 class Queue
   def initialize
+    @store = Array.new
   end
 
   def enqueue(element)
+    @store << element
   end
 
   def dequeue
+    @store.shift
   end
 
   def front
+    return @store[0]
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    size == 0
   end
 
   def to_s
+    @store.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -1,24 +1,33 @@
-class Stack
+#Stack is like a pared down array that can only use the methods we create for it
+
+class Stack  #implement something = defining how we make that thing (using or invoking is making an instance of that object)
   def initialize
     @store = Array.new
   end
 
   def push(element)
+    @store << element
   end
 
   def pop
+    @store.pop
   end
 
   def top
+    @store.last
   end
 
   def size
+    @store.length
   end
 
   def empty?
+    @store.empty?
+      # @store.length == 0
+      # size == 0 <-- crystal's choice b/c it is the most clear
   end
 
-  def to_s
-    return @store.to_s
+  def to_s #converts to string so that stack can be printed; just used for inspection
+    return @store.to_s #calls the method on the Array
   end
 end
